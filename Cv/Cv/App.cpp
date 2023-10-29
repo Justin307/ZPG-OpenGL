5390ad9d5c2799bc83aca3a6d576125354953a0b
Ondřej Just <ondrajust@seznam.cz>
2023-10-29 16:59:03 +0100

    Scene 2

--- Cv/Cv/App.cpp
+++ Cv/Cv/App.cpp
@@ -198,7 +198,7 @@ void App::run()
     camera->AttachObserver(blinn);
     camera->NotifyObservers();
 
-    PositionedLight* light = new PositionedLight(glm::vec4(0.75f, 0.75f, 0.75f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
+    PositionedLight* light = new PositionedLight(glm::vec4(1.0f, 1.0f, 1.0f, 1.0f), glm::vec3(0.0f, 0.0f, 0.0f));
     light->AttachObserver(lambert);
     light->AttachObserver(phong);
     light->AttachObserver(blinn);
@@ -208,11 +208,12 @@ void App::run()
     Material* ruby = new Material(glm::vec3(0.1745, 0.01175, 0.01175),glm::vec3(0.61424, 0.04136, 0.04136), glm::vec3(0.727811, 0.626959, 0.626959), 32);
     Material* bronze = new Material(glm::vec3(0.2125, 0.1275, 0.054),glm::vec3(0.714, 0.4284, 0.18144), glm::vec3(0.393548, 0.271906, 0.166721), 8);
     Material* silver = new Material(glm::vec3(0.19225, 0.19225, 0.19225),glm::vec3(0.50754, 0.50754, 0.50754), glm::vec3(0.508273, 0.508273, 0.508273), 16);
-    Material* brass = new Material(glm::vec3(0.329412, 0.223529, 0.027451),glm::vec3(0.780392, 0.568627, 0.113725), glm::vec3(0.992157, 0.941176, 0.807843), 8);
+    Material* brass = new Material(glm::vec3(0.329412, 0.223529, 0.027451), glm::vec3(0.780392, 0.568627, 0.113725), glm::vec3(0.992157, 0.941176, 0.807843), 8);
+    Material* turquoise = new Material(glm::vec3(0.1, 0.18725, 0.1745),glm::vec3(0.396, 0.74151, 0.69102), glm::vec3(0.297254, 0.30829, 0.306678), 4);
     
     Scene* scene = new Scene();
 
-#define SCENE 5
+#define SCENE 2
 
 #if SCENE == 1
     //Phong's four balls
@@ -226,6 +227,87 @@ void App::run()
     scene->AddModel(new DrawableObject(new Model(sphere, sizeof(sphere)), phong, emerald, transformation4));
 #elif SCENE == 2
     //Solar system
+    Model* sphereModel = new Model(sphere, sizeof(sphere));
+    scene->AddModel(new DrawableObject(sphereModel, constant, ruby));
+    //Mercury
+    TransformationComposite* mercuryTransformation = new TransformationComposite();
+    TransformationRotate* mercuryRotation = new TransformationRotate(glm::radians(0.0f), glm::vec3(0.0, 1.0, 0.0));
+    mercuryTransformation->AddTransformation(mercuryRotation);
+    mercuryTransformation->AddTransformation(new TransformationTranslate(glm::vec3(1.7, 0.0, 0.0)));
+    mercuryTransformation->AddTransformation(new TransformationScale(glm::vec3(0.1, 0.1, 0.1)));
+    scene->AddModel(new DrawableObject(sphereModel, blinn, silver, mercuryTransformation));
+    //Venus
+    TransformationComposite* venusTransformation = new TransformationComposite();
+    TransformationRotate* venusRotation = new TransformationRotate(glm::radians(0.0f), glm::vec3(0.0, 1.0, 0.0));
+    venusTransformation->AddTransformation(venusRotation);
+    venusTransformation->AddTransformation(new TransformationTranslate(glm::vec3(2.33, 0.0, 0.0)));
+    venusTransformation->AddTransformation(new TransformationScale(glm::vec3(0.2, 0.2, 0.2)));
+    scene->AddModel(new DrawableObject(sphereModel, blinn, brass, venusTransformation));
+    //Earth
+    TransformationComposite* earthTransformation = new TransformationComposite();
+    TransformationRotate* earthRotation = new TransformationRotate(glm::radians(0.0f), glm::vec3(0.0, 1.0, 0.0));
+    earthTransformation->AddTransformation(earthRotation);
+    earthTransformation->AddTransformation(new TransformationTranslate(glm::vec3(3, 0.0, 0.0)));
+    earthTransformation->AddTransformation(new TransformationScale(glm::vec3(0.22, 0.22, 0.22)));
+    scene->AddModel(new DrawableObject(sphereModel, blinn, emerald, earthTransformation));
+    //Earth Moon
+    TransformationComposite* moonTransformation = new TransformationComposite();
+    moonTransformation->AddTransformation(earthTransformation);
+    TransformationRotate* moonRotation = new TransformationRotate(glm::radians(0.0f), glm::vec3(0.0, 1.0, 0.0));
+    moonTransformation->AddTransformation(moonRotation);
+    moonTransformation->AddTransformation(new TransformationTranslate(glm::vec3(1.5, 0.0, 0.0)));
+    moonTransformation->AddTransformation(new TransformationScale(glm::vec3(0.1, 0.1, 0.1)));
+    scene->AddModel(new DrawableObject(sphereModel, blinn, ruby, moonTransformation));
+    //Mars
+    TransformationComposite* marsTransformation = new TransformationComposite();
+    TransformationRotate* marsRotation = new TransformationRotate(glm::radians(0.0f), glm::vec3(0.0, 1.0, 0.0));
+    marsTransformation->AddTransformation(marsRotation);
+    marsTransformation->AddTransformation(new TransformationTranslate(glm::vec3(4, 0.0, 0.0)));
+    marsTransformation->AddTransformation(new TransformationScale(glm::vec3(0.15, 0.15, 0.15)));
+    scene->AddModel(new DrawableObject(sphereModel, blinn, bronze, marsTransformation));
+    //Jupiter
+    TransformationComposite* jupiterTransformation = new TransformationComposite();
+    TransformationRotate* jupiterRotation = new TransformationRotate(glm::radians(0.0f), glm::vec3(0.0, 1.0, 0.0));
+    jupiterTransformation->AddTransformation(jupiterRotation);
+    jupiterTransformation->AddTransformation(new TransformationTranslate(glm::vec3(5.25, 0.0, 0.0)));
+    jupiterTransformation->AddTransformation(new TransformationScale(glm::vec3(0.45, 0.45, 0.45)));
+    scene->AddModel(new DrawableObject(sphereModel, blinn, bronze, jupiterTransformation));
+    //Saturn
+    TransformationComposite* saturnTransformation = new TransformationComposite();
+    TransformationRotate* saturnRotation = new TransformationRotate(glm::radians(0.0f), glm::vec3(0.0, 1.0, 0.0));
+    saturnTransformation->AddTransformation(saturnRotation);
+    saturnTransformation->AddTransformation(new TransformationTranslate(glm::vec3(6.2, 0.0, 0.0)));
+    saturnTransformation->AddTransformation(new TransformationScale(glm::vec3(0.30, 0.30, 0.30)));
+    scene->AddModel(new DrawableObject(sphereModel, blinn, brass, saturnTransformation));
+    //Uranus
+    TransformationComposite* uranusTransformation = new TransformationComposite();
+    TransformationRotate* uranusRotation = new TransformationRotate(glm::radians(0.0f), glm::vec3(0.0, 1.0, 0.0));
+    uranusTransformation->AddTransformation(uranusRotation);
+    uranusTransformation->AddTransformation(new TransformationTranslate(glm::vec3(7.1, 0.0, 0.0)));
+    uranusTransformation->AddTransformation(new TransformationScale(glm::vec3(0.30, 0.30, 0.30)));
+    scene->AddModel(new DrawableObject(sphereModel, blinn, turquoise, uranusTransformation));
+    //Neptune
+    TransformationComposite* neptuneTransformation = new TransformationComposite();
+    TransformationRotate* neptuneRotation = new TransformationRotate(glm::radians(0.0f), glm::vec3(0.0, 1.0, 0.0));
+    neptuneTransformation->AddTransformation(neptuneRotation);
+    neptuneTransformation->AddTransformation(new TransformationTranslate(glm::vec3(8, 0.0, 0.0)));
+    neptuneTransformation->AddTransformation(new TransformationScale(glm::vec3(0.30, 0.30, 0.30)));
+    scene->AddModel(new DrawableObject(sphereModel, blinn, turquoise, neptuneTransformation));
+    //Pluto
+    TransformationComposite* plutoTransformation = new TransformationComposite();
+    TransformationRotate* plutoRotation = new TransformationRotate(glm::radians(0.0f), glm::vec3(0.0, 1.0, 0.0));
+    plutoTransformation->AddTransformation(plutoRotation);
+    plutoTransformation->AddTransformation(new TransformationTranslate(glm::vec3(8.5, 0.0, 0.0)));
+    plutoTransformation->AddTransformation(new TransformationScale(glm::vec3(0.05, 0.05, 0.05)));
+    scene->AddModel(new DrawableObject(sphereModel, blinn, silver, plutoTransformation));
+    //Comet
+    TransformationComposite* cometTransformation = new TransformationComposite();
+    cometTransformation->AddTransformation(new TransformationScale(glm::vec3(0.15, 0.15, 0.15)));
+    cometTransformation->AddTransformation(new TransformationRotate(glm::radians(90.0f), glm::vec3(1.0, 0.0, 0.0)));
+    TransformationTranslate* cometMovement = new TransformationTranslate(glm::vec3(10.0, -60.0, -10.0));
+    cometTransformation->AddTransformation(cometMovement);
+    scene->AddModel(new DrawableObject(new Model(gift, sizeof(gift)), lambert, ruby, cometTransformation));
+
 #elif SCENE == 3 
     //Phong's light trimming
     Shader* phongwrong = Shader::LoadFromFile(std::string("..\\Cv\\shaders\\vertex.vert"), std::string("..\\Cv\\shaders\\phongwrong.frag"));
@@ -305,11 +387,11 @@ void App::run()
     Model* treeModel = new Model(tree, sizeof(tree));
     for (int i = 0; i < 120; i++)
     {
-        TransformationComposite* treeTransformation = new TransformationComposite();
         float x = (static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (30)))) - 15;
         float z = (static_cast <float> (rand()) / (static_cast <float> (RAND_MAX / (30)))) - 15;
         if ((3.0f > x && x >= -3.0f) && (3.0f > z && z >= -3.0f))
             continue;
+        TransformationComposite* treeTransformation = new TransformationComposite();
         treeTransformation->AddTransformation(new TransformationTranslate(glm::vec3(x, -1.0f, z)));
         float scale = 0.25 + static_cast <float> (rand() / (static_cast <float> (RAND_MAX)));
         treeTransformation->AddTransformation(new TransformationScale(glm::vec3(scale, scale, scale)));
@@ -324,6 +406,22 @@ void App::run()
         glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
         // render scene
         scene->Render();
+#if SCENE == 2
+        //Planets
+        mercuryRotation->angle += 0.0199;
+        venusRotation->angle += 0.0145;
+        earthRotation->angle += 0.0124;
+        marsRotation->angle += 0.0100;
+        jupiterRotation->angle += 0.0054;
+        saturnRotation->angle += 0.0040;
+        uranusRotation->angle += 0.0028;
+        neptuneRotation->angle += 0.0023;
+        plutoRotation->angle += 0.0020;
+        //Moon
+        moonRotation->angle += 0.05;
+        //Comet
+        cometMovement->value.y += 0.05;
+#endif
         // update other events like input handling
         glfwPollEvents();
         // put the stuff we’ve been drawing onto the display
